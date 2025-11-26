656fff4b306981e565bf925d7bf6cb00a48892dc
gfriess <gerhard.friess@az-armaturen.de>
2025-11-26 10:07:23 +0100

    prototyp

---
+++ app.js
@@ -0,0 +1,36 @@
+function getKeyFromUrl() {
+    const params = new URLSearchParams(window.location.search);
+    return params.get("key");
+}
+
+async function loadProduct() {
+    const key = getKeyFromUrl();
+    if(!key) return;
+
+    const url = `/data/produkt_${key}.json`;
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+
+        document.getElementById("produktbild").src = data.bild;
+        document.getElementById("typ").innerText = data.typ;
+        document.getElementById("abdichtung").innerText = data.abdichtung;
+        document.getElementById("anschluss").innerText = data.anschluss;
+        document.getElementById("dichtleiste").innerText = data.dichtleiste;
+        document.getElementById("nennweite").innerText = data.nennweite;
+        document.getElementById("nenndruck").innerText = data.nenndruck;
+        document.getElementById("baulaenge").innerText = data.baulaenge;
+
+        document.getElementById("gehaeuse").innerText = data.werkstoffe.gehaeuse;
+        document.getElementById("kueken").innerText = data.werkstoffe.kueken;
+        document.getElementById("deckel").innerText = data.werkstoffe.deckel;
+
+        document.getElementById("dichtbuchse").innerText = data.dichtbuchse;
+        document.getElementById("schrauben").innerText = data.schrauben;
+
+        document.getElementById("pdf-link").href = data.pdf;
+
+    } catch (err) {
+        document.body.innerHTML = "<h2>Produkt nicht gefunden</h2>";
+    }
+}
